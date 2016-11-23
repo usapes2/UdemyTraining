5d8e03b9edc8e24cec4c68a191d6012f56d762a9
Uladzimir Sapeshka <usapeshka@gmail.com>
2016-11-22 22:51:19 -0500

    02_package intro

---
+++ 02_package/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"fmt"
+
+	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!og, olleh"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
